7ea4f900e23ae122baeb987b2bb930b8052c758e
Martin Gontovnikas <martin@gonto.com.ar>
2013-01-30 15:00:03 -0300

    Added test for model. Section 4, video 1

---
+++ test/models/ContactTest.java
@@ -0,0 +1,34 @@
+package models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+public class ContactTest {
+
+    @Test
+    public void testContact() {
+        running(fakeApplication(inMemoryDatabase()), new Runnable() {
+            
+            @Override
+            public void run() {
+                Contact contact = new Contact();
+                contact.name = "Martin Gontovnikas";
+                contact.phone = "555111222";
+                contact.email = "g@g.com";
+                
+                contact.save();
+                
+                Contact contactFromDb = contact.find.byId(contact.id);
+                
+                assertThat(contactFromDb).isNotNull();
+                
+                assertThat(contactFromDb.name).isEqualTo(contact.name);
+            }
+        });
+    }
+
+}
