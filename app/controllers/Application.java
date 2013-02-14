2de52d0b7a85cd567cf0a83e072169f46ff5a053
Martin Gontovnikas <martin@gonto.com.ar>
2013-02-14 14:49:18 -0300

    App finished

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,25 @@
 package controllers;
 
-import play.*;
+import play.libs.F.Function;
 import play.libs.WS;
-import play.mvc.*;
-
-import views.html.*;
+import play.libs.WS.Response;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
   
   public static Result index() {
       return redirect(routes.Agenda.list());
+  }
+  
+  public static Result externalWs() {
+      return async(WS.url("http://search.twitter.com/search.json").setQueryParameter("q", "mgonto").get().map(new Function<WS.Response, Result>() {
+
+        @Override
+        public Result apply(Response response) throws Throwable {
+            return ok(response.asJson().findPath("max_id"));
+        }
+    }));
   }
   
 }
